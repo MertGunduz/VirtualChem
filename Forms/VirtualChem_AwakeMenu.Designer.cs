587a66936ec19be4aa437481387de1f02528a5f6
Mert Gunduz <merttgg@gmail.com>
2021-10-11 14:40:39 +0300

    Source Added

---
+++ Forms/VirtualChem_AwakeMenu.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace VirtualChem.Forms
+{
+    partial class VirtualChem_AwakeMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Main_Panel = new System.Windows.Forms.Panel();
+            this.RightMarginBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftMarginBorder_Panel = new System.Windows.Forms.Panel();
+            this.BottomMarginBorder_Panel = new System.Windows.Forms.Panel();
+            this.TopMarginBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.RightMargin_Panel = new System.Windows.Forms.Panel();
+            this.BottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.TopMargin_Panel = new System.Windows.Forms.Panel();
+            this.BottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.TopBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.RightBorder_Panel = new System.Windows.Forms.Panel();
+            this.Inner_Panel = new System.Windows.Forms.Panel();
+            this.LogoPanelLeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.LogoPanelRightMargin_Panel = new System.Windows.Forms.Panel();
+            this.LogoPanelBottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.LogoPanelTopMargin_Panel = new System.Windows.Forms.Panel();
+            this.Logo_Panel = new System.Windows.Forms.Panel();
+            this.Main_Panel.SuspendLayout();
+            this.Inner_Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Main_Panel
+            // 
+            this.Main_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Main_Panel.Controls.Add(this.Inner_Panel);
+            this.Main_Panel.Controls.Add(this.RightMarginBorder_Panel);
+            this.Main_Panel.Controls.Add(this.LeftMarginBorder_Panel);
+            this.Main_Panel.Controls.Add(this.BottomMarginBorder_Panel);
+            this.Main_Panel.Controls.Add(this.TopMarginBorder_Panel);
+            this.Main_Panel.Controls.Add(this.LeftMargin_Panel);
+            this.Main_Panel.Controls.Add(this.RightMargin_Panel);
+            this.Main_Panel.Controls.Add(this.BottomMargin_Panel);
+            this.Main_Panel.Controls.Add(this.TopMargin_Panel);
+            this.Main_Panel.Controls.Add(this.BottomBorder_Panel);
+            this.Main_Panel.Controls.Add(this.TopBorder_Panel);
+            this.Main_Panel.Controls.Add(this.LeftBorder_Panel);
+            this.Main_Panel.Controls.Add(this.RightBorder_Panel);
+            this.Main_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Main_Panel.Location = new System.Drawing.Point(0, 0);
+            this.Main_Panel.Name = "Main_Panel";
+            this.Main_Panel.Size = new System.Drawing.Size(400, 400);
+            this.Main_Panel.TabIndex = 0;
+            // 
+            // RightMarginBorder_Panel
+            // 
+            this.RightMarginBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.RightMarginBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMarginBorder_Panel.Location = new System.Drawing.Point(346, 54);
+            this.RightMarginBorder_Panel.Name = "RightMarginBorder_Panel";
+            this.RightMarginBorder_Panel.Size = new System.Drawing.Size(2, 292);
+            this.RightMarginBorder_Panel.TabIndex = 11;
+            // 
+            // LeftMarginBorder_Panel
+            // 
+            this.LeftMarginBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.LeftMarginBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMarginBorder_Panel.Location = new System.Drawing.Point(52, 54);
+            this.LeftMarginBorder_Panel.Name = "LeftMarginBorder_Panel";
+            this.LeftMarginBorder_Panel.Size = new System.Drawing.Size(2, 292);
+            this.LeftMarginBorder_Panel.TabIndex = 10;
+            // 
+            // BottomMarginBorder_Panel
+            // 
+            this.BottomMarginBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.BottomMarginBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMarginBorder_Panel.Location = new System.Drawing.Point(52, 346);
+            this.BottomMarginBorder_Panel.Name = "BottomMarginBorder_Panel";
+            this.BottomMarginBorder_Panel.Size = new System.Drawing.Size(296, 2);
+            this.BottomMarginBorder_Panel.TabIndex = 9;
+            // 
+            // TopMarginBorder_Panel
+            // 
+            this.TopMarginBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.TopMarginBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMarginBorder_Panel.Location = new System.Drawing.Point(52, 52);
+            this.TopMarginBorder_Panel.Name = "TopMarginBorder_Panel";
+            this.TopMarginBorder_Panel.Size = new System.Drawing.Size(296, 2);
+            this.TopMarginBorder_Panel.TabIndex = 8;
+            // 
+            // LeftMargin_Panel
+            // 
+            this.LeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMargin_Panel.Location = new System.Drawing.Point(2, 52);
+            this.LeftMargin_Panel.Name = "LeftMargin_Panel";
+            this.LeftMargin_Panel.Size = new System.Drawing.Size(50, 296);
+            this.LeftMargin_Panel.TabIndex = 7;
+            // 
+            // RightMargin_Panel
+            // 
+            this.RightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMargin_Panel.Location = new System.Drawing.Point(348, 52);
+            this.RightMargin_Panel.Name = "RightMargin_Panel";
+            this.RightMargin_Panel.Size = new System.Drawing.Size(50, 296);
+            this.RightMargin_Panel.TabIndex = 6;
+            // 
+            // BottomMargin_Panel
+            // 
+            this.BottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMargin_Panel.Location = new System.Drawing.Point(2, 348);
+            this.BottomMargin_Panel.Name = "BottomMargin_Panel";
+            this.BottomMargin_Panel.Size = new System.Drawing.Size(396, 50);
+            this.BottomMargin_Panel.TabIndex = 5;
+            // 
+            // TopMargin_Panel
+            // 
+            this.TopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMargin_Panel.Location = new System.Drawing.Point(2, 2);
+            this.TopMargin_Panel.Name = "TopMargin_Panel";
+            this.TopMargin_Panel.Size = new System.Drawing.Size(396, 50);
+            this.TopMargin_Panel.TabIndex = 4;
+            // 
+            // BottomBorder_Panel
+            // 
+            this.BottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.BottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBorder_Panel.Location = new System.Drawing.Point(2, 398);
+            this.BottomBorder_Panel.Name = "BottomBorder_Panel";
+            this.BottomBorder_Panel.Size = new System.Drawing.Size(396, 2);
+            this.BottomBorder_Panel.TabIndex = 3;
+            // 
+            // TopBorder_Panel
+            // 
+            this.TopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.TopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorder_Panel.Location = new System.Drawing.Point(2, 0);
+            this.TopBorder_Panel.Name = "TopBorder_Panel";
+            this.TopBorder_Panel.Size = new System.Drawing.Size(396, 2);
+            this.TopBorder_Panel.TabIndex = 2;
+            // 
+            // LeftBorder_Panel
+            // 
+            this.LeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.LeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftBorder_Panel.Location = new System.Drawing.Point(0, 0);
+            this.LeftBorder_Panel.Name = "LeftBorder_Panel";
+            this.LeftBorder_Panel.Size = new System.Drawing.Size(2, 400);
+            this.LeftBorder_Panel.TabIndex = 1;
+            // 
+            // RightBorder_Panel
+            // 
+            this.RightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.RightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightBorder_Panel.Location = new System.Drawing.Point(398, 0);
+            this.RightBorder_Panel.Name = "RightBorder_Panel";
+            this.RightBorder_Panel.Size = new System.Drawing.Size(2, 400);
+            this.RightBorder_Panel.TabIndex = 0;
+            // 
+            // Inner_Panel
+            // 
+            this.Inner_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.Inner_Panel.Controls.Add(this.Logo_Panel);
+            this.Inner_Panel.Controls.Add(this.LogoPanelBottomMargin_Panel);
+            this.Inner_Panel.Controls.Add(this.LogoPanelTopMargin_Panel);
+            this.Inner_Panel.Controls.Add(this.LogoPanelLeftMargin_Panel);
+            this.Inner_Panel.Controls.Add(this.LogoPanelRightMargin_Panel);
+            this.Inner_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Inner_Panel.Location = new System.Drawing.Point(54, 54);
+            this.Inner_Panel.Name = "Inner_Panel";
+            this.Inner_Panel.Size = new System.Drawing.Size(292, 292);
+            this.Inner_Panel.TabIndex = 12;
+            // 
+            // LogoPanelLeftMargin_Panel
+            // 
+            this.LogoPanelLeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LogoPanelLeftMargin_Panel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanelLeftMargin_Panel.Name = "LogoPanelLeftMargin_Panel";
+            this.LogoPanelLeftMargin_Panel.Size = new System.Drawing.Size(50, 292);
+            this.LogoPanelLeftMargin_Panel.TabIndex = 9;
+            // 
+            // LogoPanelRightMargin_Panel
+            // 
+            this.LogoPanelRightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.LogoPanelRightMargin_Panel.Location = new System.Drawing.Point(242, 0);
+            this.LogoPanelRightMargin_Panel.Name = "LogoPanelRightMargin_Panel";
+            this.LogoPanelRightMargin_Panel.Size = new System.Drawing.Size(50, 292);
+            this.LogoPanelRightMargin_Panel.TabIndex = 8;
+            // 
+            // LogoPanelBottomMargin_Panel
+            // 
+            this.LogoPanelBottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LogoPanelBottomMargin_Panel.Location = new System.Drawing.Point(50, 242);
+            this.LogoPanelBottomMargin_Panel.Name = "LogoPanelBottomMargin_Panel";
+            this.LogoPanelBottomMargin_Panel.Size = new System.Drawing.Size(192, 50);
+            this.LogoPanelBottomMargin_Panel.TabIndex = 11;
+            // 
+            // LogoPanelTopMargin_Panel
+            // 
+            this.LogoPanelTopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LogoPanelTopMargin_Panel.Location = new System.Drawing.Point(50, 0);
+            this.LogoPanelTopMargin_Panel.Name = "LogoPanelTopMargin_Panel";
+            this.LogoPanelTopMargin_Panel.Size = new System.Drawing.Size(192, 50);
+            this.LogoPanelTopMargin_Panel.TabIndex = 10;
+            // 
+            // Logo_Panel
+            // 
+            this.Logo_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Logo_Panel.Location = new System.Drawing.Point(50, 50);
+            this.Logo_Panel.Name = "Logo_Panel";
+            this.Logo_Panel.Size = new System.Drawing.Size(192, 192);
+            this.Logo_Panel.TabIndex = 12;
+            // 
+            // VirtualChem_AwakeMenu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.Main_Panel);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "VirtualChem_AwakeMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VirtualChem";
+            this.Main_Panel.ResumeLayout(false);
+            this.Inner_Panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Main_Panel;
+        private System.Windows.Forms.Panel LeftBorder_Panel;
+        private System.Windows.Forms.Panel RightBorder_Panel;
+        private System.Windows.Forms.Panel TopBorder_Panel;
+        private System.Windows.Forms.Panel BottomBorder_Panel;
+        private System.Windows.Forms.Panel BottomMargin_Panel;
+        private System.Windows.Forms.Panel TopMargin_Panel;
+        private System.Windows.Forms.Panel LeftMargin_Panel;
+        private System.Windows.Forms.Panel RightMargin_Panel;
+        private System.Windows.Forms.Panel RightMarginBorder_Panel;
+        private System.Windows.Forms.Panel LeftMarginBorder_Panel;
+        private System.Windows.Forms.Panel BottomMarginBorder_Panel;
+        private System.Windows.Forms.Panel TopMarginBorder_Panel;
+        private System.Windows.Forms.Panel Inner_Panel;
+        private System.Windows.Forms.Panel Logo_Panel;
+        private System.Windows.Forms.Panel LogoPanelBottomMargin_Panel;
+        private System.Windows.Forms.Panel LogoPanelTopMargin_Panel;
+        private System.Windows.Forms.Panel LogoPanelLeftMargin_Panel;
+        private System.Windows.Forms.Panel LogoPanelRightMargin_Panel;
+    }
+}
---
+++ Forms/VirtualChem_AwakeMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VirtualChem.Forms
+{
+    public partial class VirtualChem_AwakeMenu : Form
+    {
+        public VirtualChem_AwakeMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
